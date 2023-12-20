b1edbc3400debc2a658a23709383d6908e2f0c8b
gemramirez <geraldinemaesramirez@gmail.com>
2023-12-21 07:17:15 +0800

    Profile

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -3,43 +3,59 @@ import React from 'react';
 const DashboardCard = () => {
   return (
     <div>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-details">
-            <p className="text-title">Course 1:</p>
-            <h3 className='card-h3'>Intro to Programming</h3>
-            <p className="text-body">
+      <div className="dash-card-container">
+        <div className="dash-card">
+          <div className="dash-card-details">
+            <p className="dash-text-title">Course 1:</p>
+            <h3 className='dash-h3-card'>Intro to Programming</h3>
+            <p className="dash-text-body">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               numquam minima consectetur officiis fugit facere excepturi alias
               adipisci cumque vitae, exercitationem bce8b1 bce8b1 bce8b1 bce8b1 bce8b1 
             </p>
           </div>
-          <button className="card-button">More info</button>
+          <button className="dash-button">More info</button>
         </div> <br /><br />
-        <div className="card">
-          <div className="card-details">
-            <p className="text-title">Course 1:</p>
-            <h3 className='card-h3'>Intro to Programming</h3>
-            <p className="text-body">
+
+        <div className="dash-card">
+          <div className="dash-card-details">
+            <p className="dash-text-title">Course 1:</p>
+            <h3 className='dash-h3-card'>Intro to Programming</h3>
+            <p className="dash-text-body">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               numquam minima consectetur officiis fugit facere excepturi alias
               adipisci cumque vitae, exercitationem bce8b1 bce8b1 bce8b1 bce8b1 bce8b1 
             </p>
           </div>
-          <button className="card-button">More info</button>
+          <button className="dash-button">More info</button>
         </div> <br /><br />
-        <div className="card">
-          <div className="card-details">
-            <p className="text-title">Course 1:</p>
-            <h3 className='card-h3'>Intro to Programming</h3>
-            <p className="text-body">
+
+        <div className="dash-card">
+          <div className="dash-card-details">
+            <p className="dash-text-title">Course 1:</p>
+            <h3 className='dash-h3-card'>Intro to Programming</h3>
+            <p className="dash-text-body">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
               numquam minima consectetur officiis fugit facere excepturi alias
               adipisci cumque vitae, exercitationem bce8b1 bce8b1 bce8b1 bce8b1 bce8b1 
             </p>
           </div>
-          <button className="card-button">More info</button>
+          <button className="dash-button">More info</button>
         </div> <br /><br />
+
+        <div className="dash-card">
+          <div className="dash-card-details">
+            <p className="dash-text-title">Course 1:</p>
+            <h3 className='dash-h3-card'>Intro to Programming</h3>
+            <p className="dash-text-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              numquam minima consectetur officiis fugit facere excepturi alias
+              adipisci cumque vitae, exercitationem bce8b1 bce8b1 bce8b1 bce8b1 bce8b1 
+            </p>
+          </div>
+          <button className="dash-button">More info</button>
+        </div> <br /><br />
+       
       </div>
     </div>
   );
