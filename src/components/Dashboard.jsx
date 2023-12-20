b045a2759e1cde366246d1b4040ea14e8fe9f9a1
gemramirez <geraldinemaesramirez@gmail.com>
2023-12-20 14:21:00 +0800

    commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,12 @@
-import React from 'react'
+import React from 'react';
 import sample from '../assets/prog.png';
-import sample1 from '../assets/prog.png';
-import sample2 from '../assets/prog.png';
 const Dashboard = () => {
   return (
-   
-   <div>
-    <div className='Upper'>
-      background and picture
-    </div>
-    <div className="card">
-      <img src="sample" src={sample} alt="sa" />
-      <img src="sample" src={sample1} alt="sa" />
-      <img src="sample" src={sample2} alt="sa" />
+    <div>
+      <div className="card">
+        <img src="sample" src={sample} alt="sa" />
       </div>
     </div>
   );
 };
-export default Dashboard
-
-
+export default Dashboard;
